4de463f82c0125797d33efe2759095583ce9a25d
Tilemachos Spanos <tilemachosspanos@Tilemachoss-MacBook-Pro.local>
2023-06-17 13:01:04 +0300

    final commit

--- todo/urls.py
+++ todo/urls.py
@@ -1,22 +1,17 @@
 from django.urls import path
 from . import views
-from django.urls import reverse 
+from django.urls import reverse
 
 
 urlpatterns = [
-
     # Add Task Feature
-    path('addTask/', views.addTask, name='addTask'),
-
+    path("addTask/", views.addTask, name="addTask"),
     # Mark as Done Feature
-    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
-
+    path("mark_as_done/<int:pk>/", views.mark_as_done, name="mark_as_done"),
     # Mark as Undone Feature
-    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
-
+    path("mark_as_undone/<int:pk>/", views.mark_as_undone, name="mark_as_undone"),
     # Edit Feature
-    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
-
+    path("edit_task/<int:pk>/", views.edit_task, name="edit_task"),
     # Delete Feature
-    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+    path("delete_task/<int:pk>/", views.delete_task, name="delete_task"),
 ]
